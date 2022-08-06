948b0cce5d790241ec189c5e7ccef65b894a7001
Henward <Henward@Henrys-MBP-3.attlocal.net>
2022-08-06 17:17:50 -0500

    refactored naming of parameters

--- main.go
+++ main.go
@@ -12,52 +12,54 @@ func main() {
 	http.HandleFunc("/Get", GetRooms)
 	http.HandleFunc("/Leave", LeaveRoom)
 	http.HandleFunc("/SwipeRight", SwipeRight)
-  	http.HandleFunc("/SwipeLeft", SwipeLeft)
+	http.HandleFunc("/SwipeLeft", SwipeLeft)
 	http.ListenAndServe(":42069", nil)
 }
 
-func Alive(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+// response  is the response back to user
+// r is the request we recieve
+func Alive(response http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(response, "Hello, I'm still alive!") // prints string to response
 }
 
-//wat do: add user to a room in database
-//wat need: user id, room id
-//return: list of restaurants
-func JoinRoom(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+// wat do: add user to a room in database
+// wat need: user id, room id
+// return: list of restaurants
+func JoinRoom(response http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(response, "Hello, %s!", request.URL.Path[1:])
 }
 
-//wat do: creates a room
-//wat need: location, user
-//return: list of restaurants, room ID
-func CreateRoom(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+// wat do: creates a room
+// wat need: location, user
+// return: list of restaurants, room ID
+func CreateRoom(response http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(response, "Hello, %s!", request.URL.Path[1:])
 }
 
-//wat do: list all current rooms
-//wat need: nothing
-//return: list of all rooms
-func GetRooms(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+// wat do: list all current rooms
+// wat need: nothing
+// return: list of all rooms
+func GetRooms(response http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(response, "Hello, %s!", request.URL.Path[1:])
 }
 
-//wat do: remove user from room
-//wat need: user id, room id they're leaving from
-//return: nothing
-func LeaveRoom(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+// wat do: remove user from room
+// wat need: user id, room id they're leaving from
+// return: nothing
+func LeaveRoom(response http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(response, "Hello, %s!", request.URL.Path[1:])
 }
 
-//wat do: increment vote for selected restaurant
-//wat need: restaurant id
-//return: true or false for found
-func SwipeRight(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+// wat do: increment vote for selected restaurant
+// wat need: restaurant id
+// return: true or false for found
+func SwipeRight(response http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(response, "Hello, %s!", request.URL.Path[1:])
 }
 
-//wat do: nothing
-//wat need:
-//return: true or false for found
-func SwipeLeft(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+// wat do: nothing
+// wat need:
+// return: true or false for found
+func SwipeLeft(response http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(response, "Hello, %s!", request.URL.Path[1:])
 }
